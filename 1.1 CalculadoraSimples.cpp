001d2dfdc606880bcccb888b880d96c34cebe9db
HigorLopesZimerman <higorlzimerman@gmail.com>
2025-06-09 02:59:51 -0300

    Update 1.1 CalculadoraSimples.cpp
    Refiz o código usando POO

--- 1.1 CalculadoraSimples.cpp
+++ 1.1 CalculadoraSimples.cpp
@@ -1,87 +1,95 @@
 #include <iostream>
 #include <cstdlib>
 
-void adicao(float x, float y, float &z){
-	system("cls");
-	z = x + y;
-	std::cout << "\nResultado de " << x << " + " << y <<": " << z << std::endl << std::endl;
-}
-
-void subtracao(float x, float y, float &z){
-	system("cls");
-	z = x - y;
-	std::cout << "\nResultado de " << x << " - " << y <<": " << z << std::endl << std::endl;
-}
-
-void mult(float x, float y, float &z){
-	system("cls");
-	z = x * y;
-	std::cout << "\nResultado de " << x << " x " << y <<": " << z << std::endl << std::endl;
-}
-
-void div(float x, float y, float &z){
-	system("cls");
-	z = x / y;
-	std::cout << "\nResultado de " << x << " / " << y <<": " << z << std::endl << std::endl;
-}
-
-int main(){
+class Calculadora {
 	
-	float x, y, z;
+	private:
+		float x, y, z;
 	
-	
-	
-	std::cout << "Digite o primeiro numero(X): ";
-	std::cin >> x;
-	system("cls");
-	std::cout << "Digite o segundo numero(Y): ";
-	std::cin >> y;
-	system("cls");
-	
-	
-	int option;
-	
-	do{
-		
-		std::cout << "\n====Operacoes====\n";
-		std::cout << "1. Adicao\n";
-		std::cout << "2. Subtracao\n";
-		std::cout << "3. Multiplicacao\n";
-		std::cout << "4. Divisao\n";
-		std::cout << "0. Sair\n";
-		std::cout << "\nInsira uma das opcoes: ";
-		std::cin >> option;
-		
-		
-		switch(option){
+	public:
+		void lerNumeros(){
+			std::cout << "Digite o primeiro numero(X): ";
+			std::cin >> x;
+			system("cls");
 			
-			case 1:{
-				adicao(x, y, z);
-				break;
-			}
-			
-			case 2:{
-				subtracao(x, y, z);
-				break;
-			}
-			
-			case 3:{
-				mult(x, y, z);
-				break;
-			}
-			
-			case 4:{
-				div(x, y, z);
-				break;
-			}
-			
-			case 0:{
-				std::cout << "Saindo da calculadora...";	
-				break;
-			}
+			std::cout << "Digite o segundo numero(Y): ";
+			std::cin >> y;
+			system("cls");
 		}
 		
-	} while(option!=0);
+		
+		void menuOperacoes() {
+        	int option;
 	
+        	do {
+	            std::cout << "\n==== Operacoes ====\n";
+	            std::cout << "1. Adicao\n";
+	            std::cout << "2. Subtracao\n";
+	            std::cout << "3. Multiplicacao\n";
+	            std::cout << "4. Divisao\n";
+	            std::cout << "0. Sair\n";
+	            std::cout << "\nInsira uma das opcoes: ";
+	            std::cin >> option;
 	
+	            switch (option) {
+	                case 1:
+	                    adicao();
+	                    break;
+	                case 2:
+	                    subtracao();
+	                    break;
+	                case 3:
+	                    multiplicacao();
+	                    break;
+	                case 4:
+	                    divisao();
+	                    break;
+	                case 0:
+	                    std::cout << "Saindo da calculadora..." << std::endl;
+	                    break;
+	                default:
+	                    std::cout << "Opcao invalida!" << std::endl;
+	            }
+	
+	        } while (option != 0);
+	    }
+	    
+	    private:
+	    	void adicao(){
+				system("cls");
+				z = x + y;
+				std::cout << "\nResultado de " << x << " + " << y <<": " << z << std::endl << std::endl;
+			}
+	    	
+	    	void subtracao(){
+				system("cls");
+				z = x - y;
+				std::cout << "\nResultado de " << x << " - " << y <<": " << z << std::endl << std::endl;
+			}
+			
+			void multiplicacao(){
+				system("cls");
+				z = x * y;
+				std::cout << "\nResultado de " << x << " x " << y <<": " << z << std::endl << std::endl;
+			}
+			
+			void divisao(){
+				system("cls");
+				if(y!=0){
+				z = x / y;
+				std::cout << "\nResultado de " << x << " / " << y <<": " << z << std::endl << std::endl;
+				} else {
+					std::cout << "\nErro: Divisao por zero!" << std::endl << std::endl;
+				}
+			}
+		
+};
+
+int main(){
+	Calculadora calc;
+	
+	calc.lerNumeros();
+	calc.menuOperacoes();
+	
+	return 0;
 }
